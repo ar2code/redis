332578b679c8fd6820e73b67469885decdaf56ee
Alexey Rozhkov <alexey.rojkov@gmail.com>
2021-07-29 08:43:39 +0300

    error check methods

--- redis-core-android/src/main/java/ru/ar2code/redis/core/android/RedisErrorViewState.kt
+++ redis-core-android/src/main/java/ru/ar2code/redis/core/android/RedisErrorViewState.kt
@@ -1,14 +1,24 @@
 package ru.ar2code.redis.core.android
 
 import ru.ar2code.redis.core.State
+import kotlin.reflect.KClass
 
 interface RedisErrorViewState : RedisViewState {
     val error: Changeable<State.ErrorOccurred>
 
     //todo test
-    fun isErrorShouldBeRender(currentUiState: RedisErrorViewState?): Boolean {
-        return error.data != null && error.version != currentUiState?.error?.version
+    fun isErrorShouldBeRendered(currentUiState: RedisErrorViewState?): Boolean {
+        return error.shouldBeRendered(currentUiState?.error)
+    }
+
+    fun isErrorThrowableInstanceOf(errorType: KClass<out Throwable>): Boolean {
+        return error.data?.throwable != null && errorType.isInstance(error.data?.throwable)
     }
 
     fun updateErrorVersion(serviceError: State.ErrorOccurred): RedisErrorViewState
+
+    /**
+     * @return true if error is not specific for the current viewState, and will not be rendered in a special way.
+     */
+    fun isUnhandledError(): Boolean
 }
